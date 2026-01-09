ba3960355622cc7a144f5ea0b323b33d6858e93f
everviera12 <evertigre10@gmail.com>
2026-01-09 17:26:18 -0600

    continuous learning

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
+<script setup>
+console.log("Hello world from console");
+const message = 'Hello world dinamyc'
+const active = false
+const modal = true
+
+const arrayFruits = ['🍑', '🍍', '🍏', '🍉', '🍓', '🍌', '🍊']
+</script>
+
 <template>
   <h1>Hello world!</h1>
+  <h2>{{ message }}</h2>
+  <!-- <h3 :style="`color: red; font-weight: 600;`">{{ active ? 'tamo activo 😎' : 'no tamo activo 😖' }}</h3> -->
+  
+  <!-- directivas -->
+  <h3 v-if="active">tamo activo con directivas 😎</h3>
+  <h3 v-else="active">no tamo activo con directivas 😖</h3>
+
+  <h4 v-show="modal" style="color: yellow;"> v-show {{ modal }}</h4>
 </template>
+
+<style>
+h1 {
+  color: dodgerblue;
+}
+</style>
